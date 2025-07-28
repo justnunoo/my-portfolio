262c50ab66a5c2c3a2047767d428c14d67082ee7
Justice Samuel Nii Akwah Nunoo <justnunoo@gmail.com>
2025-07-28 23:28:34 +0000

    Added new project: Urban Phlick studios website.

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -39,6 +39,14 @@ export default function ProjectHome() {
     },
     {
       id: 3,
+      title: "Urban Phlick Studios",
+      description: "A portfolio website for a photography studio showcasing their work and also to allow users to book for a studio session.",
+      tags: ["Next.js", "Node.js", "Tailwind CSS"],
+      image: "/images/urban_phlick_studios.png",
+      link: "https://urban-phlick-photography.vercel.app/"
+    },
+    {
+      id: 4,
       title: "Dataco Sales Analysis Project",
       description: "A data analysis of the year to year sales of a fictional company.",
       tags: ["SQL", "Tableau"],
@@ -46,7 +54,7 @@ export default function ProjectHome() {
       link: "https://public.tableau.com/views/datacosalesdashboard/Dashboard1?:language=en-GB&publish=yes&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link"
     },
     {
-      id: 4,
+      id: 5,
       title: "Potato Disease Detection",
       description: "In this project, I trained a machine learning model, to detect potato plant diseases using an image of it's leave. ",
       tags: ["Python", "Tensorflow", "CNN", "OpenCV"],
@@ -54,7 +62,7 @@ export default function ProjectHome() {
       link: "https://huggingface.co/spaces/justnunoo/potato-disease-detector"
     },
     {
-      id: 5,
+      id: 6,
       title: "Football Stars Facial Recognition",
       description: "This a facial recognition and classification project. The model was trained on the images of certain very popular footballing stars like Lionel Messi, Neymar, Ronaldo, Mbappe, and Lamine Yamal.",
       tags: ["Python", "Tensorflow", "CNN", "OpenCV"],
@@ -62,7 +70,7 @@ export default function ProjectHome() {
       link: "https://huggingface.co/spaces/justnunoo/football-player-classifier"
     },
     {
-      id: 6,
+      id: 7,
       title: "AI Generated Exams",
       description: "An AI-powered application that generates exam questions based on a given text which also accepts text files.",
       tags: ["NextJS", "NodeJS", "Groq AI", "Tailwind CSS"],
@@ -70,7 +78,7 @@ export default function ProjectHome() {
       link: "https://jsn-quiz-app.vercel.app/"
     },
     {
-        id: 7,
+        id: 8,
         title: "IntoWine Consult Website",
         description: "A website for a start-up financial consulting company.",
         tags: ["Next.js", "Tailwind CSS"],
@@ -78,7 +86,7 @@ export default function ProjectHome() {
         link: "https://felix-app.vercel.app"
     },
     {
-      id: 8,
+      id: 9,
       title: "New York Housing Sales Dashboard",
       description: "A dashboard to visualize sales of homes in New York and other insights.",
       tags: ["SQL", "Tableau"],
@@ -86,7 +94,7 @@ export default function ProjectHome() {
       link: "https://public.tableau.com/views/nyc-rolling-sales_17085235198880/Dashboard1?:language=en-GB&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link"
     },
     {
-      id: 9,
+      id: 10,
       title: "Portfolio Website",
       description: "A responsive portfolio website built with Next.js.",
       tags: ["Next.js", "Tailwind CSS"],
@@ -94,7 +102,7 @@ export default function ProjectHome() {
       link: "https://jsn-my-portfolio.vercel.app/projects"
     },
     {
-      id: 10,
+      id: 11,
       title: "Global Cybersecurity Threats Dashboard",
       description: "A dashboard to visualize global cybersecurity threats.",
       tags: ["SQL", "Tableau", "Python"],
